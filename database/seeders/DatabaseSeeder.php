5e41c51fa385afa26f629e509ae0a511f8d62866
devsquad-eduardo-lima <eduardo.lima@devsquad.com>
2022-07-28 17:27:59 -0400

    create modal user and user seeder default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Contrato\Contrato;
 use App\Models\Propriedade\Propriedade;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,9 +18,16 @@ class DatabaseSeeder extends Seeder
     {
         Propriedade::factory()->count(100)->create();
 
-        for($i = 0; $i <= 20; $i++)
-        {
+        for ($i = 0; $i <= 20; $i++) {
             Contrato::factory()->create();
         }
+
+        User::updateOrCreate(
+            ['email' => 'teste@example.com'],
+            [
+                'name' => 'Usuário de Teste',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]
+        );
     }
 }
